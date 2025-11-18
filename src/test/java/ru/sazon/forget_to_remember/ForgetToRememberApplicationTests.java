d1c6b2df1136d04d049f09c02bf507d1e26b9d9a
Alexandr <onepunch.zone@yandex.ru>
2025-11-19 03:44:38 +0500

    start

---
+++ src/test/java/ru/sazon/forget_to_remember/ForgetToRememberApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.sazon.forget_to_remember;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForgetToRememberApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
